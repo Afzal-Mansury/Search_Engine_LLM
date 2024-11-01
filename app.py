fb2281dc4bca3a633fc8306cb126b0a0a5be43a4
Afzal Mansury <60620241+Afzal-Mansury@users.noreply.github.com>
2024-11-01 20:47:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
 
-# create the Wrapper
+# create the Wrapper | update
 arxiv_wrapper = ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=250)
 arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
 
